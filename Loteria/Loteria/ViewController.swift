e96b999afc062443292f8cb2d72377f519f328da
Antonio Jesus <antoniojesus.cazalla@babel.es>
2022-03-02 19:27:13 +0100

    Create project

---
+++ Loteria/Loteria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by antoniojesus.cazalla.local on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ande andas")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
